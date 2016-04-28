b3f4d3797107076c529d3ca05db2b212b2611caf
David Luu <luu.david.c@gmail.com>
2016-04-28 09:42:48 -0700

    Zipcode-crime chart

--- public/js/map.js
+++ public/js/map.js
@@ -1,13 +1,13 @@
 var parentWidth = $('#map').width(),
-    parentHeight = window.innerHeight - ($('.navbar').height() / 2);
+    parentHeight = window.innerHeight;
 
-var margin = ((parentWidth > parentHeight) ? parentWidth : parentHeight) / 25,
+var margin = 50,
     width = parentWidth - (2 * margin),
     height = parentHeight - (2 * margin);
 
 var svg = d3.select('#map').append('svg')
-    .attr('width', width + margin)
-    .attr('height', height + margin);
+    .attr('width', width)
+    .attr('height', height);
 
 
 $.getJSON('/data/sd.json', function(sd) {
@@ -17,7 +17,7 @@ $.getJSON('/data/sd.json', function(sd) {
   /* Calculate the center of sd, guess scale and offset */
   var center = d3.geo.centroid(sdgeo);
   var scale = 150;
-  var offset = [width/2, height/2 - ($('.navbar').height() / 2)];
+  var offset = [width/2, height/2];
   var projection = d3.geo.mercator()
                       .scale(scale)
                       .center(center)
@@ -144,10 +144,10 @@ function changeNeighborhoods(displayName, crimes) {
     var data = map[displayName];
     if (data) {
       if (data.cost == -1) {
-        $('#cost').html("No cost information for " + displayName + "!");
+        $('#cost').html("No house cost information for " + displayName + "!");
       }
       else {
-        $('#cost').html("<strong>Cost:</strong> $" + data.cost.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+        $('#cost').html("<strong>House Cost:</strong> $" + data.cost.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
       }
     }
     else {
@@ -155,6 +155,26 @@ function changeNeighborhoods(displayName, crimes) {
     }
 
     $('#crime').html("<strong>Total Crimes:</strong> " + crimes);
+
+    var query = [
+      "SELECT zip,COUNT(*)",
+      "FROM cogs121_16_raw.arjis_crimes AS c"
+    ];
+
+    for (var i in data.zipcodes) {
+      if (i == 0) {
+        query.push("WHERE c.zip LIKE '" + data.zipcodes[i] + "'");
+      }
+      else {
+        query.push("OR c.zip LIKE '" + data.zipcodes[i] + "'");
+      }
+    }
+
+    query.push("GROUP BY zip");
+
+    $.getJSON(generateQueryURL('/delphidata', query), function(data) {
+      updateChart(data, '#zipcode-crime');
+    });
   });
 }
 
@@ -203,20 +223,61 @@ function handleFill(d) {
   }
 }
 
-/*
-  subplaces = topojson.feature(sd, sd.objects.neighborhoods);
 
-  var projection = d3.geo.mercator();
-  var path = d3.geo.path().projection(projection);
+function updateChart (data, id) {
+  $(id).empty();
 
-  var features = subplaces.features;
-  for (var i = 0; i < features.length; i++) {
-      var feat = features[i];
-      
-      // calculate the centroid (which is in pixels) and then invert back to lat/long
-      var centroid = projection.invert(path.centroid(feat));
+  var range = d3.max( data.map(function(d){ return parseInt(d.count); }) ) + 100;
 
-      // output: id, long, lat
-      console.log('{name: "' + feat.properties.name + '", centroid:[' + centroid[1] + ', ' + centroid[0] + ']},');
-  }
-*/
+  var margin = {top: 0, right: 0, bottom: 50, left: 60},
+      width = $('#crime').width() - margin.right - margin.left,
+      height = ($(window).height() - $('#zipcode-crime').position().top) / 2.1 - margin.top - margin.bottom;
+
+  var innerWidth  = width  - margin.left - margin.right;
+  var innerHeight = height - margin.top  - margin.bottom;
+
+  var xScale = d3.scale.ordinal().rangeRoundBands([0, innerWidth], 0);
+  var yScale = d3.scale.linear().range([0, innerHeight]);
+
+  var chart = d3
+                .select(id)
+                .append("svg")
+                .attr("width", width + margin.right + margin.left)
+                .attr("height", height + margin.top + margin.bottom)
+                .append("g")
+                .attr("transform", "translate(" +  margin.left + "," + margin.right + ")");
+
+  xScale.domain( data.map(function (d){ return d.zip; }) );
+
+  yScale.domain([range,0]);
+  
+  chart
+    .selectAll(".bar")
+    .data(data.map(function(d){ return d.count; }))
+    .enter().append("rect")
+    .attr("class", "bar")
+    .attr("x", function(d, i) { return ((innerWidth / data.length)*i) + 5; })
+    .attr("width", (innerWidth / data.length) - 10)
+    .attr("y", function(d) { return innerHeight - (innerHeight*(d / range)); })
+    .attr("height", function(d) { return innerHeight*(d / range); })
+    .style("fill", "#aaa");
+
+  var xAxis = d3.svg.axis().scale(xScale).orient("bottom");
+  var yAxis = d3.svg.axis().scale(yScale).orient("left");
+
+  chart
+    .append("g")
+      .attr("transform", "translate(" + 0 + "," + innerHeight + ")")
+      .call(xAxis)
+    .selectAll("text")
+      .attr("transform",
+        "translate(" + 0 + "," + 10 + ")" +
+        " " +
+        "rotate(" + -45 + ")"
+      )
+      .style("text-anchor", "end");
+
+  chart
+    .append("g")
+      .call(yAxis);
+}
