501bb9549eac84a6393a0c25f4f80c38c27b6f46
David Luu <luu.david.c@gmail.com>
2016-04-27 23:17:35 -0700

    Front page form submitting; Initial map selection

--- public/js/index.js
+++ public/js/index.js
@@ -11,37 +11,13 @@ function generateQueryURL(base, query) {
   return queryURL;
 }
 
-/* On Ready */
-(function(d3) {
-  'use strict';
-
-  /* Query strings sorted in an array */
-  var query = [
-    "SELECT COUNT(*)",
-    "FROM cogs121_16_raw.arjis_crimes AS c",
-    "WHERE NOT(c.zip LIKE '')"
-  ];
-
-  d3.json(generateQueryURL('/delphidata', query), function(err, data) {
-    console.log(data);
-    if (err) {
-      console.error(err);
-      return;
-    }
-
-    d3.json('/data/zipcodeToNeighborhood.json', function(err, map) {
-      if (err) {
-        console.error(err);
-        return;
-      }
-
-      console.log();
-
-      data.forEach(function(data){
-        if (map[data.zip]) {
-
-        }
-      });
-    });
+$.get('/autocomp', function(response) {
+  $('#srch-term').autocomplete({
+    source: response.cities.neighborhood
   });
-})(d3);
+});
+
+/* On Ready */
+$(function() {
+
+});
